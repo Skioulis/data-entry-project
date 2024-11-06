13087465b5474fddc173af2884a08a328591bd3c
Skioulis <skioulis@gmail.com>
2024-11-06 20:09:10 +0200

    some testing

--- main.py
+++ main.py
@@ -13,4 +13,6 @@ zillow_clone_url="https://appbrewery.github.io/Zillow-Clone/"
 
 scrape= Scraping()
 
+for i in range(len(scrape.prices)):
+    print(f"{i}. - the building in this address: {scrape.addresses[i]} with price: {scrape.prices[i]} and link: {scrape.listings[i]}")
 
