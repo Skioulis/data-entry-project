a9996d007b91d7eb1fc43f83d2a5c4c656b1a2a0
Fotis Fotiadis <skioulis@gmail.com>
2024-11-06 12:13:31 +0200

    did some tidying

--- scraping.py
+++ scraping.py
@@ -7,20 +7,17 @@ header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gec
 
 class Scraping:
     def __init__(self):
+        self.soup = self.get_soup()
         self.listings = self.get_the_urls()
         self.prices = []
         self.addresses = []
 
 
-
-    def get_the_urls(self, url: str = zillow_clone_url):
+    def get_soup(self, url: str = zillow_clone_url):
         response = requests.get(url=url, headers=header)
         response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
+        return BeautifulSoup(response.text, 'html.parser')
 
-        houses = soup.select("ul.List-c11n-8-84-3-photo-cards li")
-        # print(len(houses))
-        url_list = soup.select("a.property-card-link")
-        # print(len(self.listings))
-        # print(self.listings[0].get_attribute_list("href"))
+    def get_the_urls(self):
+        url_list = self.soup.select("a.property-card-link")
         return [url.get('href') for url in url_list]
